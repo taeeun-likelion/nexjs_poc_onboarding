569b7010a803a85cb5dcec548a7e1e3c8aa09f89
Taeeun Kim <taeeunkim@likelion.net>
2021-11-18 15:48:09 +0900

    :lipstick:  style: set profile

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
 export default function Home() {
   return (
-    <h1 className="title">Read{' '} {/*used to divided text over lines*/}
-    <Link href="/posts/first-post">
-      <a>this page!</a>
-    </Link>
-    </h1>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>My name is Taeeun Kim, and I am a junior web frontend developer.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   )
 }
